af630c4955fd3a14badcc42fc56c44ac6a7c32c7
Lorenzo Quintana <lquintana25@gmail.com>
2017-12-03 11:37:32 -0700

    AboutMe java code
    Author: LQuintana
    Created Date: Nov 19, 2017

---
+++ AboutMe.java
@@ -0,0 +1,14 @@
+/**Program:About Me program
+ * File:AboutMe.java
+ * Summary:Prints out simple info about my the author
+ * Author:Lorenzo Quintana
+ * Date:Nov 19,2017
+ */
+
+public class AboutMe {
+	public static void main(String[]args){
+		System.out.println("My Name is Lorenzo Quintana");
+		System.out.println("My Favorite Color is Blue");
+		System.out.println("If I won the lottery, I would pay off all my debts");
+	}
+}
